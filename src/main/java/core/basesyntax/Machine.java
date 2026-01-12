c683d379201302876078c6b2d75c2aa6adb4d842
Mariia Makohin <maria.makohin2420@gmail.com>
2026-01-12 14:28:29 +0200

    change mistakes1

--- src/main/java/core/basesyntax/Machine.java
+++ src/main/java/core/basesyntax/Machine.java
@@ -1,6 +1,8 @@
 package core.basesyntax;
 
 public abstract class Machine {
+
     abstract void doWork();
+
     abstract void stopWork();
 }
--- src/main/java/core/basesyntax/Truck.java
+++ src/main/java/core/basesyntax/Truck.java
@@ -1,12 +1,14 @@
 package core.basesyntax;
 
 public class Truck extends Machine {
-    public void doWork () {
+
+    public void doWork() {
         System.out.println("Truck start work");
     }
 
     public void stopWork() {
         System.out.println("Truck stop work");
     }
+
 
 }
