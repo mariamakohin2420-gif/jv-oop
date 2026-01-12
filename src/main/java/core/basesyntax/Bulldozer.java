e7a4944436c65577127c0bf38f9c33bf5b64c3ff
Mariia Makohin <maria.makohin2420@gmail.com>
2026-01-12 13:03:03 +0200

    Implemented solution

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+class Bulldozer extends Machine {
+    void doWork () {
+        System.out.println("Bulldozer start work");
+    }
+    void stopWork() {
+        System.out.println("Bulldozer stop work");
+    }
+}
---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+class Truck extends Machine {
+    void doWork () {
+        System.out.println("Truck start work");
+    }
+    void stopWork() {
+        System.out.println("Truck stop work");
+    }
+
+}
