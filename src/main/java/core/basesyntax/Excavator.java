2fbae928c6be475b118b4a3af4b85e8878dbd892
Mariia Makohin <maria.makohin2420@gmail.com>
2026-01-12 14:15:16 +0200

    check  new mistakes

--- src/main/java/core/basesyntax/Excavator.java
+++ src/main/java/core/basesyntax/Excavator.java
@@ -1,11 +1,11 @@
 package core.basesyntax;
 
 public class Excavator extends Machine {
-    void doWork () {
+    public void doWork () {
         System.out.println("Excavator start work");
     }
 
-    void stopWork() {
+    public void stopWork() {
         System.out.println("Excavator stop work");
     }
 }
